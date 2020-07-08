f94f06c4bf908925f692d4d0e021b5be7f6ce35c
Handell Desulme <tue73811@temple.edu>
2020-07-07 20:29:32 -0400

    completes day two exercises

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,10 +10,18 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
-
+import os
+here = os.path.dirname(os.path.abspath(__file__))
+filename = os.path.join(here, 'foo.txt')
+with open(filename) as foo:
+    contents = foo.read()
+    print(contents)
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+filename = 'bar.txt'
+with open(filename, 'w') as bar:
+    bar.write("Line one. \nLine two. \nLine three.")
--- src/15_classes.py
+++ src/15_classes.py
@@ -2,28 +2,46 @@
 # constructor
 
 # YOUR CODE HERE
+class LatLon:
+    def __init__(self, lat, lon):
+        self.latitude = lat
+        self.longitude = lon
 
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 
 # YOUR CODE HERE
-
+class Waypoint(LatLon):
+    def __init__(self, lat, lon, name):
+        super().__init__(lat, lon)
+        self.wpname = name
+    def __str__(self):
+        return "Name: "+self.wpname+"\nLatitude: "+str(self.latitude)+"\nLongitude: "+str(self.longitude)
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
 # YOUR CODE HERE
+class Geocache(Waypoint):
+    def __init__(self, lat, lon, name, difficulty, size):
+        super().__init__(lat, lon, name)
+        self.gdifficulty = difficulty
+        self.gsize = size
+    def __str__(self):
+        return "Name: "+self.wpname+"\nLatitude: "+str(self.latitude)+"\nLongitude: "+str(self.longitude)+"\nDifficulty: "+str(self.gdifficulty)+"\nSize: "+str(self.gsize)
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
 # YOUR CODE HERE
+waypoint = Waypoint(41.70505,-121.51521,"Catacombs")
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
 print(waypoint)
+print()
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
 # YOUR CODE HERE
-
+geocache = Geocache(44.052137, -121.41556, "Newberry Views", 1.5, 2)
 # Print it--also make this print more nicely
 print(geocache)
